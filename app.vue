ede6201f6ca3d5889c5dbb0d1ad62579b0bafe5a
fikilifadly <fikilifadly@gmail.com>
2024-05-19 19:31:41 +0700

    feat: add skeleton

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<HeroBanner />
+	</div>
+</template>
+
+<script setup>
+import HeroBanner from "./src/components/HeroBanner.vue";
+import "./src/assets/css/global.sass";
+</script>
